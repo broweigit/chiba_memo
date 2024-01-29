dcedc72d8e44bb7b0b4c7bbac59b38d119d62972
broweigit <broweigg@gmail.com>
2024-01-30 01:56:10 +0800

    HelloFramer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./app.scss"
+import Navbar from "./components/navbar/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <div>
+    <section id="Homepage">
+      <Navbar/>
+    </section>
+    <section id="Parallax">Parallax</section>
+    <section id="Service">Service</section>
+    <section id="Portfolio">Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section id="Contact">Contact</section>
+  </div>;
+};
 
 export default App;
